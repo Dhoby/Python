c39ae6ce2b83127ff7c0c011fac6e50bc3dee9ef
Dhoby <151893019+Dhoby@users.noreply.github.com>
2024-01-05 00:41:48 +0800

    upload

---
+++ Essential Math for Date Science/Example 1-27. Another integral approximation in Python/E1.27.py
@@ -0,0 +1,3 @@
+area = approximate_integral(a=0, b=1, n=1000, f=my_function)
+
+print(area) # prints 1.333333250000001
---
+++ Essential Math for Date Science/Example 1-28. Yet another integral approximation in Python/E1.28.py
@@ -0,0 +1,2 @@
+area = approximate_integral(a=0, b=1, n=1_000_000, f=my_function)
+print(area) # prints 1.3333333333332733
