20da26f7c7ab220274aaed9dda4f2f5bf51dd3d8
Dhoby <151893019+Dhoby@users.noreply.github.com>
2024-01-26 15:41:22 +0800

    upload

--- Dhoby Codes/stockprofit.py
+++ Dhoby Codes/stockprofit.py
@@ -1,12 +1,15 @@
-ORI = 18.6 * 500
-COM_INI = max (18.6 * 500 * 2.5 / 10000, 5)
+BUY = input("what is the initial price? 18.6 for myself\n") 
+ORI = float (BUY)* 500
+COM_INI = max (ORI * 2.5 / 10000, 5)
+GUOHU_INI = ORI * 0.01 / 1000
+
 PRICE = input ("what is the price now?\n")
 COM_SOLD = max (float (PRICE) * 500 * 2.5 / 10000, 5)
-Profit = float (PRICE) * 500 - ORI - COM_INI - COM_SOLD
-print ("your profit is: ")
-print ('%.2f'%Profit)
-print ("your initial commission is: ")
-print ('%.2f'%COM_INI)
-print ("your sold commission is: ")
-print ('%.2f'%COM_SOLD)
-print ("\n")
+GUOHU_SOLD = float (PRICE) * 500 * 0.01 / 1000
+STAMP = float (PRICE) * 500 * 0.05 / 100
+Profit = float (PRICE) * 500 - ORI - COM_INI - COM_SOLD - GUOHU_INI - GUOHU_SOLD - STAMP
+
+COST = COM_INI + COM_SOLD + GUOHU_INI + GUOHU_SOLD + STAMP
+print ("the total cost is: " + '%.2f'%COST)
+
+print ("your net profit is: " + '%.2f'%Profit)
