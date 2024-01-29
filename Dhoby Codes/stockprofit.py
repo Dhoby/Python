eec160543b0f66ac2744be7354b5ea36091a6248
Dhoby <151893019+Dhoby@users.noreply.github.com>
2024-01-29 15:24:13 +0800

    upload

--- Dhoby Codes/stockprofit.py
+++ Dhoby Codes/stockprofit.py
@@ -1,15 +1,30 @@
-BUY = input("what is the initial price? 18.61 for myself\n") 
-ORI = float (BUY)* 500
-COM_INI = max (ORI * 2.5 / 10000, 5)
-GUOHU_INI = ORI * 0.01 / 1000
 
-PRICE = input ("what is the price now?\n")
-COM_SOLD = max (float (PRICE) * 500 * 2.5 / 10000, 5)
-GUOHU_SOLD = float (PRICE) * 500 * 0.01 / 1000
-STAMP = float (PRICE) * 500 * 0.05 / 100
-Profit = float (PRICE) * 500 - ORI - COM_INI - COM_SOLD - GUOHU_INI - GUOHU_SOLD - STAMP
+TYP = input("how many types of stocks?\n")
+NUM = int (TYP)
 
-COST = COM_INI + COM_SOLD + GUOHU_INI + GUOHU_SOLD + STAMP
-print ("the total cost is: " + '%.2f'%COST)
+i = 0
+TOT_Profit = 0
 
-print ("your net profit is: " + '%.2f'%Profit)
+while i < NUM: 
+    QTY = input("how many do you carry?\n")
+    HOL = int (QTY)
+    BUY = input("what is the initial price? 18.61 for myself\n") 
+    ORI = float (BUY)* HOL
+    COM_INI = max (ORI * 2.5 / 10000, 5)
+    GUOHU_INI = ORI * 0.01 / 1000
+
+    PRICE = input ("what is the price now?\n")
+    COM_SOLD = max (float (PRICE) * HOL * 2.5 / 10000, 5)
+    GUOHU_SOLD = float (PRICE) * HOL * 0.01 / 1000
+    STAMP = float (PRICE) * HOL * 0.05 / 100
+    Profit = float (PRICE) *HOL - ORI - COM_INI - COM_SOLD - GUOHU_INI - GUOHU_SOLD - STAMP
+
+    COST = COM_INI + COM_SOLD + GUOHU_INI + GUOHU_SOLD + STAMP
+    print ("all cost for this stock is: " + '%.2f'%COST)
+
+    print ("your net profit for this stock is: " + '%.2f'%Profit)
+    print("/n")
+    TOT_Profit = TOT_Profit + Profit
+    i = i + 1;
+    
+print ("All your net profit is: " + '%.2f'%TOT_Profit)
